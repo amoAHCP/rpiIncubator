07a87607c4e97e278e07daaf3270d21c6b4e5d83
amoAHCP <amo.ahcp@gmail.com>
2015-04-20 22:08:53 +0200

    initial commit

---
+++ rpiVertx-sensors/src/main/java/ch/trivadis/com/util/WebSocketRepository.java
@@ -0,0 +1,42 @@
+package ch.trivadis.com.util;
+
+import io.vertx.core.http.ServerWebSocket;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * A WebSocket repositry to store all connected sockets for each verticle instance. Note, each instance has it's own classloder. Synchronize your communication between socket and verticle instances.
+ * Created by Andy Moncsek on 20.04.15.
+ */
+public class WebSocketRepository {
+
+    private List<ServerWebSocket> webSockets = new CopyOnWriteArrayList<>();
+
+    /**
+     * Add a server socket
+     *
+     * @param webSocket
+     */
+    public void addWebSocket(ServerWebSocket webSocket) {
+        webSockets.add(webSocket);
+    }
+
+    /**
+     * Get all WebSockets for verticle instance
+     *
+     * @return
+     */
+    public List<ServerWebSocket> getWebSockets() {
+        return Collections.unmodifiableList(webSockets);
+    }
+
+    /**
+     * remove a server socket
+     * @param webSocket
+     */
+    public void removeWebSocket(ServerWebSocket webSocket) {
+        webSockets.remove(webSocket);
+    }
+}
---
+++ rpiVertx-sensors/src/main/java/ch/trivadis/com/verticle/TemperatureSensor.java
@@ -0,0 +1,75 @@
+package ch.trivadis.com.verticle;
+
+
+import ch.trivadis.com.util.WebSocketRepository;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.ServerWebSocket;
+import io.vertx.core.json.JsonObject;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by Andy Moncsek on 20.04.15.
+ */
+public class TemperatureSensor extends AbstractVerticle {
+    private String url=  "temperature";
+    private final String URL_DEFAULT=  "temperature";
+    private final WebSocketRepository repository = new WebSocketRepository();
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+
+    public static void main(String[] args) {
+        System.out.println("sddfgd");
+    }
+
+    @Override
+    public void start() throws Exception {
+        initConfiguration(config());
+        vertx.createHttpServer().websocketHandler(ws -> {
+            if (ws.path().toLowerCase().equals(url)) {
+                ws.handler((event)->currentTemperatureHandler(ws,event));
+                ws.endHandler((event) -> repository.removeWebSocket(ws));
+                ws.closeHandler((event)-> repository.removeWebSocket(ws));
+                repository.addWebSocket(ws);
+
+            }
+
+
+        }).listen(8080);
+
+        vertx.setPeriodic(5000,(eventTime)->
+            CompletableFuture.
+                    supplyAsync(() -> readTemperatureFromDevice(), executor).
+                    thenAccept((temperature) -> repository.getWebSockets().forEach(ws -> ws.writeMessage(Buffer.buffer().appendString(temperature))))
+        );
+    }
+
+
+
+    private void currentTemperatureHandler(final ServerWebSocket ws, final Buffer buffer) {
+        // ignore (request) buffer data
+        CompletableFuture.
+                supplyAsync(() -> readTemperatureFromDevice(), executor).
+                thenAccept((temperature) -> ws.writeMessage(Buffer.buffer().appendString(temperature)));
+    }
+
+
+
+    private String readTemperatureFromDevice() {
+        // read data from ??whatever ??
+        System.out.println("read ");
+        return "24";
+    }
+
+    /**
+     * init configuration from config file or take default values
+     * @param config
+     */
+    private void initConfiguration(JsonObject config) {
+        url = config.getString("wsPath", URL_DEFAULT);
+    }
+
+
+}
